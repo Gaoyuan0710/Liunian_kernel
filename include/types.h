69f4f1767f3ed962706c2d1e0c1285df0b8442af
Gaoyuan0710 <sishuiliunian0710@gmail.com>
2014-10-10 14:02:22 +0800

    add file types.h

---
+++ include/types.h
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  types.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年10月10日 14时01分52秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Gaoyuan, sishuiliunian0710@gmail.com
+ *        Company:  Class 1204 of Computer Science and Technology
+ *
+ * =====================================================================================
+ */
+
+
